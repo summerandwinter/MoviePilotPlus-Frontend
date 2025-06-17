22b3abc9faf6878a0c5416758c2a876e0b4b8eb5
summerandwinter <ly1989124423@163.com>
2025-06-18 06:52:14 +0800

    style(CollectDetailView): 调整标签和标题的样式及条件渲染
    - 对齐标签内的文本缩进
    - 将名称显示改为标题显示并添加字体样式

--- src/views/collect/CollectDetailView.vue
+++ src/views/collect/CollectDetailView.vue
@@ -375,7 +375,7 @@ watch(() => addForm.value.tags,
             <span
               class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full whitespace-nowrap transition !no-underline bg-green-500 bg-opacity-80 border border-green-500 !text-green-100 hover:bg-green-500 hover:bg-opacity-100 false overflow-hidden">
               <div class="relative z-20 flex items-center false"><span>{{ getCollectStatus(collectDetail.status)
-                  }}</span>
+              }}</span>
               </div>
             </span>
           </div>
@@ -388,7 +388,7 @@ watch(() => addForm.value.tags,
             </div>
           </h1>
           <span class="media-attributes">
-            <span v-if="collectDetail.name">{{ collectDetail.name }}</span>
+            <span v-if="collectDetail.title" class="font-title font-semibold">{{ collectDetail.title }}</span>
           </span>
         </div>
         <div class="media-actions">
