15113294b95c3f5c45611b4deb717e606bfb96a0
summerandwinter <ly1989124423@163.com>
2025-10-07 07:50:53 +0800

    feat(iqiyi): 重构爱奇艺视图页面并添加分类筛选功能
    重构爱奇艺视图页面，使用新的分类筛选组件替代原有简单筛选
    添加分类字典数据文件支持多维度筛选
    实现搜索功能并与分类筛选联动

--- src/views/collect/IQiyiView.vue
+++ src/views/collect/IQiyiView.vue
@@ -1,107 +1,196 @@
 <script setup lang="ts">
-import MediaCardListView from '@/views/collect/MediaCardListView.vue'
+import api from '@/api'
+import { ref, reactive, computed, watch, onMounted } from 'vue'
+import type { CategoryInfo, CategoryItem } from '@/api/types'
+import { default as MediaCardListView } from '@/views/collect/MediaCardListView.vue'
+import { default as MediaSearchView } from '@/views/collect/MediaSearchView.vue'
+import { VTextField } from 'vuetify/components'
 
-// 过滤参数
-const filterParams = reactive({
-  type: 2,
-  cat: null,
-  sort: 'rank', // date/rank
-  year: null,
-})
-
-// Bangumi cat字典
-/**
- * 0 为 其他
-1 为 TV
-2 为 OVA
-3 为 Movie
-5 为 WEB
- */
-const bangumiCatDict = {
-  '0': '其他',
-  '1': 'TV',
-  '2': 'OVA',
-  '3': 'Movie',
-  '5': 'WEB',
-}
-
-// Bangumi排序字典
-const bangumiSortDict = {
-  rank: '排名',
-  date: '日期',
-}
-
-// 年份字典，自动生成最近10年
-const yearDict: Record<number, number> = {}
-const currentYear = new Date().getFullYear()
-for (let i = 0; i < 10; i++) {
-  yearDict[currentYear - i] = currentYear - i
-}
-
+// 排序 类型 资费 出品 地区 年份 状态 画风 年龄 全部 性别 语言  动画明星 剧场 奖项 其他-characteristic
+// 电影或者电视剧 movies/tvs
+const type = ref('1009')
 // 当前Key
 const currentKey = ref(0)
 
-// 类型和过滤参数变化后重新刷新列表
-watch([filterParams], () => {
+const cates = ref<Record<string, CategoryInfo[]>>({
+  // 初始化一个空对象，实际数据会通过 API 获取
+})
+// 搜索词
+const searchWord = ref<string | null>(null)
+const isSearch = ref(false)
+
+
+// 过滤参数
+const defaultType = '1009'
+const defaultSort = ''
+const cate = ref('TV')
+
+// 存储每个组的选中值
+const groupSelections = reactive<Record<string, string>>({})
+
+// 根据组的选中值计算 filterParams
+const filterParams = computed(() => {
+  const params: Record<string, string> = {
+    'type': type.value,
+    "mode": '24',
+    "three_category_id_v2": '',
+    "market_release_date_level": '',
+    "smart_tag": '',
+    "smart_tag_v2": '',
+    "structure_id": '',
+    "is_purchase": '',
+    "is_album_finished": '',
+    "is_limit_free": '',
+    "is_exclusive": '',
+    "is_qiyi_produced": '',
+    "charge_control_paymark": '',
+  }
+
+  // 遍历所有组的选中值
+  Object.entries(groupSelections).forEach(([groupKey, selectedValue]) => {
+    // 确保该组存在并且选中值不为空
+    if (cates.value[groupKey] && selectedValue) {
+      // 找到对应的选项
+      const selectedOption = cates.value[groupKey].find(option => option.option_value === selectedValue)
+      if (selectedOption && selectedOption.filter_key) {
+
+        // 处理 option_value 中包含逗号的情况，如 "1_1_1,is_purchase=1"
+        if (selectedOption.option_value.indexOf(',') !== -1) {
+          const originalOptionValue = selectedOption.option_value
+          const mainValue = originalOptionValue.split(',')[0]
+
+          // 设置主参数
+          params[selectedOption.filter_key] = mainValue
+
+          // 处理逗号后的所有部分，这些是其他 filter_params 的键值对
+          const otherParams = originalOptionValue.split(',').slice(1)
+          otherParams.forEach(param => {
+            const [paramKey, paramValue] = param.split('=')
+            if (paramKey && paramValue && paramKey in params) {
+              params[paramKey] = paramValue
+            }
+          })
+        } else {
+          // 直接设置参数
+          params[selectedOption.filter_key] = selectedValue
+        }
+      }
+    }
+  })
+
+  return params
+})
+
+// 分类字典
+const cateDictArray: CategoryItem[] = [
+  { "value": "全部", "key": "1009", "cate": "TV" },
+  { "value": "电视剧", "key": "2", "cate": "TV" },
+  { "value": "电影", "key": "1", "cate": "Movie" },
+  { "value": "综艺", "key": "6", "cate": "Show" },
+  { "value": "动漫", "key": "4", "cate": "Comic" },
+  { "value": "少儿", "key": "15", "cate": "Movie" },
+  { "value": "纪录片", "key": "3", "cate": "Documentary" },
+  { "value": "微剧", "key": "35", "cate": "TV" }
+]
+// 分类信息
+async function queryCate(type: string) {
+  try {
+    const data: CategoryInfo[] = await api.get('iqiyi/category', {
+      params: {
+        type: type
+      }
+    })
+    cateDictArray.forEach(item => {
+      if (item.key == type) {
+        cate.value = item.cate
+      }
+    })
+
+    const groupedData: Record<string, CategoryInfo[]> = {};
+    data.forEach((item: CategoryInfo) => {
+      const filter_key = item.index_name;
+      if (!groupedData[filter_key]) {
+        groupedData[filter_key] = [];
+      }
+      groupedData[filter_key].push(item);
+    });
+
+    cates.value = groupedData;
+  } catch (error) {
+    console.log(error)
+  }
+}
+function searchMedia() {
+  isSearch.value = true
+}
+function searchClear() {
+  searchWord.value = null
+  isSearch.value = false
+}
+// 当组选择变化时，更新currentKey以触发数据刷新
+watch(groupSelections, () => {
+  currentKey.value++
+}, { deep: true })
+
+// 检查选项是否被选中
+function isOptionSelected(option: CategoryInfo, groupKey: string): boolean {
+  return groupSelections[groupKey] === option.option_value
+}
+
+// 获取芯片颜色
+function getChipColor(option: CategoryInfo, groupKey: string): string {
+  return isOptionSelected(option, groupKey) ? 'primary' : ''
+}
+onMounted(() => {
+  queryCate(defaultType)
+})
+// 类型变化
+watch(type, () => {
+  // 类型变化时，清空所有组的选中值
+  Object.keys(groupSelections).forEach(key => {
+    delete groupSelections[key]
+  })
+
+  queryCate(type.value)
   currentKey.value++
 })
+
+
 </script>
 
 <template>
-  <div class="px-3">
-    <div class="flex justify-start align-center">
-      <div class="mr-5">
-        <VLabel>类别</VLabel>
-      </div>
-      <VChipGroup v-model="filterParams.cat">
-        <VChip
-          :color="filterParams.cat == key ? 'primary' : ''"
-          filter
-          tile
-          :value="key"
-          v-for="(value, key) in bangumiCatDict"
-          :key="key"
-        >
-          {{ value }}
-        </VChip>
-      </VChipGroup>
-    </div>
-    <div class="flex justify-start align-center">
-      <div class="mr-5">
-        <VLabel>排序</VLabel>
-      </div>
-      <VChipGroup v-model="filterParams.sort">
-        <VChip
-          :color="filterParams.sort == key ? 'primary' : ''"
-          filter
-          tile
-          :value="key"
-          v-for="(value, key) in bangumiSortDict"
-          :key="key"
-        >
-          {{ value }}
-        </VChip>
-      </VChipGroup>
-    </div>
-    <div class="flex justify-start align-center">
-      <div class="mr-5">
-        <VLabel>年份</VLabel>
-      </div>
-      <VChipGroup v-model="filterParams.year">
-        <VChip
-          :color="filterParams.year == key ? 'primary' : ''"
-          filter
-          tile
-          :value="key"
-          v-for="(value, key) in yearDict"
-          :key="key"
-        >
-          {{ value }}
-        </VChip>
-      </VChipGroup>
-    </div>
-  </div>
   <div>
-    <MediaCardListView :key="currentKey" apipath="discover/bangumi" :params="filterParams" />
+    <div class="px-3 flex justify-start align-center">
+      <VCombobox ref="searchWordInput" v-model="searchWord" density="comfortable" variant="outlined"
+        class="search-input" prepend-inner-icon="mdi-magnify" append-inner-icon="mdi-close"
+        @click:append-inner="searchClear()" placeholder="搜索腾讯视频" @keydown.enter="searchMedia()" hide-details />
+    </div>
+    <div class="px-3" v-show="!isSearch">
+      <div class="flex justify-start align-center">
+        <VChipGroup v-model="type" column mandatory>
+          <!-- 遍历数组 -->
+          <VChip :color="type == item.key ? 'primary' : ''" tile :value="item.key" size="small"
+            v-for="item in cateDictArray" :key="item.key">
+            {{ item.value }}
+          </VChip>
+        </VChipGroup>
+      </div>
+      <div class="flex justify-start align-center" v-for="(item, key) in cates" :key="key">
+        <VChipGroup v-model="groupSelections[key]" column mandatory>
+          <VChip :color="getChipColor(option, key)" tile :value="option.option_value" v-for="option in item"
+            :key="option.option_value" size="small">
+            {{ option.option_name }}
+          </VChip>
+        </VChipGroup>
+      </div>
+    </div>
+
+
+    <div class="pt-3">
+      <MediaSearchView v-if="isSearch" :key="currentKey" :apipath="`iqiyi/search`" :keyword="searchWord || ''"
+        :cate="cate" />
+      <MediaCardListView v-show="!isSearch" :key="currentKey" :apipath="`iqiyi/page_data`" :params="filterParams"
+        :first-page="1" :cate="cate" />
+    </div>
   </div>
 </template>
